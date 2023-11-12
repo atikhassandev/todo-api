e755824cb0949703f2efd8c3bb982c33b132a85a
Atik Hassan <atikhassan.cse@gmail.com>
2023-11-12 20:10:56 +0600

    Create TaskResource to transform data for api response

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'taskId' => $this->id,
+            'title' => $this->title,
+            'status' => $this->status,
+            'image' => $this->image ? url("/storage/{$this->image}") : '',
+        ];
+    }
+}
