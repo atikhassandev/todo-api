cfef6a079508f4bd39773ffd12e2950dbb3829a5
Atik Hassan <atikhassan.cse@gmail.com>
2023-11-12 19:37:37 +0600

    cast status attribute

---
+++ app/Models/Task.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'status' => TaskStatus::class,
+    ];
+
+}
